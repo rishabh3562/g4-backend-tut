89d548f9019b851e725d57bfd5cb055a9a90963d
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-11-21 11:41:06 +0530

    auth 2 p1: registerhandler

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,24 @@
-const registerHandler = (req, res) => {
+const User = require("../models/userModel");
 
+const registerHandler = async (req, res) => {
+    // 1
+    const { name, age, email, password, role } = req.body;
+
+    // 3
+    const data = new User({
+        name: name,
+        age: age,
+        email: email,
+        password: password,
+        role: role
+    })
+    const savedUser = await data.save();
+    // 4
+    res.json({
+        success: true,
+        data: savedUser
+    })
+    // 5
 }
 const loginHandler = (req, res) => {
 
@@ -13,9 +32,9 @@ const meHandler = (req, res) => {
 
 
 module.exports = {
-     registerHandler,
-     loginHandler,
-     logoutHandler,
-     meHandler
+    registerHandler,
+    loginHandler,
+    logoutHandler,
+    meHandler
 
- }
+}
