e56916c75b05f6e9d23686e179d8a4d002842fbc
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-11-24 10:56:54 +0530

    auth 3 bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,24 @@
-    const mongoose = require('mongoose');
-    const userSchema = mongoose.Schema({
-        name: String,
-        age: Number,
-        email :String,
-        password:String,
-        role:{
-            type:String ,
-            enum:['admin','user']
-        }
-    })
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const userSchema = mongoose.Schema({
+    name: String,
+    age: Number,
+    email: String,
+    password: String,
+    role: {
+        type: String,
+        enum: ['admin', 'user']
+    }
+})
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(this.password, salt);
+    this.password = hash;
+    next();
+})
+const User = mongoose.model("Users", userSchema)
 
-    const User = mongoose.model("Users", userSchema)
-
-    module.exports = User;
+module.exports = User;
