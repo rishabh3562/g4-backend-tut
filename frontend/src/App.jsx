f1ae71ce6791b5d3e0cb00be035a0e2a2b84541b
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-12-01 11:57:31 +0530

    backend and frontend integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [data, setdata] = useState([]);
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
-export default App
+  const fetchUsers = async () => {
+    try {
+      let res = await axios.get("http://localhost:8000/api/v1/users/");
+      console.log(res);
+      setdata(res.data);
+    } catch (error) {
+      console.log("some error occurred", error);
+    }
+  };
+  return (
+    <>
+      <div>dafsadf</div>
+    </>
+  );
+};
+
+export default App;
